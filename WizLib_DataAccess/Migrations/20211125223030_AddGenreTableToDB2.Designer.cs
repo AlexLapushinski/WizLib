abfa5d9fb7c94a286e7fd78503e34a9b819cf2a5
AlexLapushinski <alex.lapushinski@quorumsoftware.com>
2021-11-25 15:31:09 -0700

    Add Genre table again

--- WizLib_DataAccess/Migrations/20211125223030_AddGenreTableToDB2.Designer.cs
+++ WizLib_DataAccess/Migrations/20211125223030_AddGenreTableToDB2.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WizLib_DataAccess.Data;
 
 namespace WizLib_DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211125223030_AddGenreTableToDB2")]
+    partial class AddGenreTableToDB2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
