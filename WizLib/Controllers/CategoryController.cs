90b0a29205771a10f2699203145bde559f7d039c
AlexLapushinski <alex.lapushinski@quorumsoftware.com>
2021-12-10 11:00:54 -0700

    Added add/edit/remove Category with Razor cshtml

---
+++ WizLib/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WizLib_DataAccess.Data;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objList = _db.Categories.ToList();
+            return View(objList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Category obj = new Category();
+            if (id == null)
+            {
+                return View(obj);
+            }
+            obj = _db.Categories.FirstOrDefault(u => u.Category_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Category_Id == 0)
+                {
+                    // create
+                    _db.Categories.Add(obj);
+                }
+                else
+                {
+                    // update
+                    _db.Categories.Update(obj);
+                }
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int id)
+        {            
+            var obj = _db.Categories.FirstOrDefault(u => u.Category_Id == id);
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
