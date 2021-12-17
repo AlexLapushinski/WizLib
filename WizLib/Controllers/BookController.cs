1efd9bbece07cc5f397191a7da72ab4acc296cf4
AlexLapushinski <alex.lapushinski@quorumsoftware.com>
2021-12-17 14:17:16 -0700

    Added book-authors many to many methods

--- WizLib/Controllers/BookController.cs
+++ WizLib/Controllers/BookController.cs
@@ -122,6 +122,55 @@ namespace WizLib.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult ManageAuthors(int id)
+        {
+            BookAuthorVM obj = new BookAuthorVM
+            {
+                BookAuthorList = _db.BookAuthors.Include(u => u.Author).Include(u => u.Book).Where(u => u.Book_Id == id).ToList(),
+                BookAuthor = new BookAuthor
+                {
+                    Book_Id = id
+                },
+                Book = _db.Books.FirstOrDefault(u => u.Book_Id == id)
+
+            };
+            List<int> tempListOfAssignedAuthors = obj.BookAuthorList.Select(u => u.Author_Id).ToList();
+            // not in clause in LINQ, authors whos ID not in tempListOfAssignedAuthors
+            var tempList = _db.Authors.Where(u => !tempListOfAssignedAuthors.Contains(u.Author_Id)).ToList();
+
+            obj.AuthorList = tempList.Select(i => new SelectListItem
+            {
+                Text = i.FullName,
+                Value = i.Author_Id.ToString()
+            });
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult ManageAuthors(BookAuthorVM bookAuthorVM)
+        {
+            if (bookAuthorVM.BookAuthor.Book_Id != 0 && bookAuthorVM.BookAuthor.Author_Id != 0)
+            {
+                _db.BookAuthors.Add(bookAuthorVM.BookAuthor);
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(ManageAuthors), new { @id = bookAuthorVM.BookAuthor.Book_Id });
+        }
+
+        [HttpPost]
+        public IActionResult RemoveAuthors(int authorId, BookAuthorVM bookAuthorVM)
+        {
+            int bookId = bookAuthorVM.Book.Book_Id;
+            BookAuthor bookAuthor = _db.BookAuthors.FirstOrDefault(
+                u => u.Author_Id == authorId && u.Book_Id == bookId);
+            _db.BookAuthors.Remove(bookAuthor);
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(ManageAuthors), new { @id = bookId });
+        }
+
         public IActionResult PlayGround()
         {
             var bookTemp = _db.Books.FirstOrDefault();
@@ -164,7 +213,7 @@ namespace WizLib.Controllers
 
             var BookTemp2 = _db.Books.Include(b => b.BookDetail).FirstOrDefault(b => b.Book_Id == 2);
             BookTemp2.BookDetail.Weight = 22;
-            _db.Attach(BookTemp2);
+            _db.Attach(BookTemp2);                    // Attach is more effective than Update
             _db.SaveChanges();
 
             return RedirectToAction(nameof(Index));
