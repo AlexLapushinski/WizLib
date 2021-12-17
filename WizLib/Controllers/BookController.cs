5b1ab85927b50ec92952a548609e49f569afb501
AlexLapushinski <alex.lapushinski@quorumsoftware.com>
2021-12-17 14:29:13 -0700

    Added authors on Book page

--- WizLib/Controllers/BookController.cs
+++ WizLib/Controllers/BookController.cs
@@ -23,15 +23,21 @@ namespace WizLib.Controllers
 
         public IActionResult Index()
         {
-            List<Book> objList = _db.Books.Include(u => u.Publisher).ToList(); // best way including Publishers
-            //foreach(var obj in objList)
-            //{
-            //    // not good way, many selects to DB
-            //    //obj.Publisher = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == obj.Publisher_id);
+            //List<Book> objList = _db.Books.Include(u => u.Publisher).ToList(); // best way including Publishers
+            List<Book> objList = _db.Books.ToList();
+            foreach (var obj in objList)
+            {
+                // not good way, many selects to DB
+                //obj.Publisher = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == obj.Publisher_id);
 
-            //    // better way with less ways to DB:
-            //    _db.Entry(obj).Reference(u => u.Publisher).Load();
-            //}
+                // better way with less ways to DB:
+                _db.Entry(obj).Reference(u => u.Publisher).Load();
+                _db.Entry(obj).Collection(u => u.BookAuthors).Load();
+                foreach(var bookAuth in obj.BookAuthors)
+                {
+                    _db.Entry(bookAuth).Reference(u => u.Author).Load();
+                }
+            }
             return View(objList);
         }
 
